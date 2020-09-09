347c1825b51845c7eb6529567b800d32c82c91d0
Chanhee <chjeong530@gmail.com>
2020-09-09 12:02:31 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,65 +1,33 @@
 import React from 'react';
 import PropTypes from "prop-types"
 
-function Food({name, rating}) {
-  return (
-    <div>
-      <h3>I like {name} </h3>
-      <h4> {rating} </h4>
-    </div>
-  );
-}
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-}
 
-const foodILike = [
-  {
-    id:1,
-    name: "A",
-    image:"",
-    rating: 5
-  },
-  {
-    id:2,
-    name: "B",
-    image:"",
-    rating: 3
-  },
-  {
-    id:3,
-    name: "C",
-    image:"",
-    rating: 4
-  },
-  {
-    id:4,
-    name: "D",
-    image:"",
-    rating: 2
-  },
-  {
-    id:5,
-    name: "E",
-    image:"",
-    rating: 3
+class App extends React.Component{
+  state = {
+    count: 0
+  };
+
+  add = () => {
+    // this.setState({count: this.state.count+1})
+    this.setState(current => ({count: current.count+1}));
+  };
+  minus = () => {
+    // this.setState({count: this.state.count-1})
+    this.setState(current => ({count: current.count-1}));
+  };
+
+
+  render() {
+    return (
+      <div>
+        <h1> The number is: {this.state.count} </h1>
+        <button onClick={this.add}> Add </button>
+        <button onClick={this.minus}> Minus </button>
+      </div>
+    );
   }
-]
 
-function renderFood(dish){
-  return ( 
-    <Food key={dish.id} name={dish.name} rating={dish.rating} />
-  )
-}
-
-function App() {
-  return( 
-    <div> Hello 
-      {foodILike.map(renderFood)}
-    </div>
-  );
 }
 
 export default App;
