baf2af5c213e1980d81ac521a1542e51ae7e953a
Chanhee <chjeong530@gmail.com>
2020-09-09 10:34:15 +0900

    PropTypes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,63 @@
 import React from 'react';
+import PropTypes from "prop-types"
 
-function Food({name}) {
+function Food({name, rating}) {
   return (
-    <h3>I like {name}</h3>
+    <div>
+      <h3>I like {name} </h3>
+      <h4> {rating} </h4>
+    </div>
   );
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
 }
 
 const foodILike = [
   {
+    id:1,
     name: "A",
-    image:""
+    image:"",
+    rating: 5
   },
   {
+    id:2,
     name: "B",
-    image:""
+    image:"",
+    rating: 3
   },
   {
+    id:3,
     name: "C",
-    image:""
+    image:"",
+    rating: 4
   },
   {
+    id:4,
     name: "D",
-    image:""
+    image:"",
+    rating: 2
   },
   {
+    id:5,
     name: "E",
-    image:""
+    image:"",
+    rating: 3
   }
 ]
+
+function renderFood(dish){
+  return ( 
+    <Food key={dish.id} name={dish.name} rating={dish.rating} />
+  )
+}
 
 function App() {
   return( 
     <div> Hello 
-      {foodILike.map(dish => <Food name={dish.name}/>)}
+      {foodILike.map(renderFood)}
     </div>
   );
 }
