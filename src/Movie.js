dfe5d8c0bbdd5ab73e1df16904e82f0ceaac794c
Chanhee Jeong <chjeong530@gmail.com>
2020-12-22 15:28:49 +0900

    add genres

--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css"
+import { getRoles } from "@testing-library/react";
 
-function Movie({id, year, title, summary, poster}){
-  return <h4>{title}</h4>
+function Movie({id, year, title, summary, poster, genres}){
+  return (
+
+    <div class="movies">
+        <img src={poster} alt={title} title={title}></img>
+      <div class="movie__data">
+        <h3 class="movie__title">{title}</h3>
+        <h5 class="movie__year">{year}</h5>
+        <ul class="genre">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genre">{genre}</li>
+          ))}
+        </ul>
+        <p class="movie__summary">{summary}</p>
+      </div>
+    </div>
+  )
 }
 
 Movie.propTypes = {
@@ -10,7 +27,8 @@ Movie.propTypes = {
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
-  poster: PropTypes.string.isRequired
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired
 }
 
 
